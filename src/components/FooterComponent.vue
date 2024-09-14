f91f17c32a521d339cd91b19890f91f408ab4d70
Letícia Monteiro <emaildeestudos90@gmail.com>
2024-09-14 00:33:27 -0300

    Alterações

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,7 +1,9 @@
 <template>
     <footer>
-        <p>©2024, UnBConnect.</p>
+        <div class="infos">
+        <p>©2024, Papelaria LaLe.</p>
         <p>Laryssa Felix e Letícia Monteiro - Programação Orientada a Objetos, 2024/1.</p>
+        </div>
     </footer>
 </template>
 
@@ -12,10 +14,17 @@
 
 footer {
     width: 100%;
+    height: 130px;
     text-align: center;
-    background-color:  #720372;
-    padding: 26px 0;
+    background-color:  #4B0082;
+    padding: 44px 0;
     color:#FFFFFF;
+    z-index: 1;
 }
 
+.infos {
+    justify-content: center;
+    align-items: center;
+    margin-top: 60px;
+}
 </style>
